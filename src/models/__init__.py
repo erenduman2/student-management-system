154ee44074e7e7fdc30fefe943c1e41ee67a92b4
erenduman2 <l1119034@std.yildiz.edu.tr>
2024-11-25 23:17:02 +0300

    deleted unnecessary repository files

--- src/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import Lecturer, Student, Course, student_course, Base
-
-__all__ = ["Lecturer", "Student", "Course", "student_course", "Base"]
--- src/repositories/__init__.py
+++
@@ -1,5 +0,0 @@
-from .course_repository import CourseRepository
-from .lecturer_repository import LecturerRepository
-from .student_repository import StudentRepository
-
-__all__ = ["CourseRepository", "LecturerRepository", "StudentRepository"]
