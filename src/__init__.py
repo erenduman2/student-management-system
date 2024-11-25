600db3ec5be9954508d03c502c91014fe8f82d5a
erenduman2 <l1119034@std.yildiz.edu.tr>
2024-11-25 23:17:31 +0300

    models are added to init file

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .database import db_session
+from .models import Lecturer, Student, Course, student_course, Base
 
-__all__ = ["db_session"]
+__all__ = ["db_session", "Lecturer", "Student", "Course", "student_course", "Base"]
